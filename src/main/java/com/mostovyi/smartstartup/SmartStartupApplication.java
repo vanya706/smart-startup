c20ae0b4ec174ae9b36a3d45b34b3f9e56a4e4f1
Ivan Mostovyi <ivan.mostovyi.work@gmail.com>
2022-08-21 01:47:39 +0300

    Init Spring Boot

---
+++ src/main/java/com/mostovyi/smartstartup/SmartStartupApplication.java
@@ -0,0 +1,13 @@
+package com.mostovyi.smartstartup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStartupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartStartupApplication.class, args);
+    }
+
+}
