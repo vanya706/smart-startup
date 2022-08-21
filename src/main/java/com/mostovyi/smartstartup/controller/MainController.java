5a7c31f8107fd5a75c8cebac452ef1aae5a7bbf6
Ivan Mostovyi <ivan.mostovyi.work@gmail.com>
2022-08-21 13:35:38 +0300

    Partial implemented JavaFX UI

--- src/main/java/com/mostovyi/smartstartup/controller/MainController.java
+++ src/main/java/com/mostovyi/smartstartup/controller/MainController.java
@@ -1,7 +1,11 @@
 package com.mostovyi.smartstartup.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +14,24 @@ import org.springframework.stereotype.Component;
 public class MainController {
 
     @FXML
-    private Label welcomeText;
-
+    private TilePane flowsTilePane;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to Smart-Startup with JavaFX and Spring Boot Application!");
+    private VBox profilesVBox;
+    @FXML
+    private VBox programsVBox;
+
+    public void createFlow(ActionEvent actionEvent) {
+
+        ToggleButton button = new ToggleButton();
+        button.setText("click");
+        Label name = new Label();
+        name.setText("flow name");
+        Label profile = new Label();
+        profile.setText("profile name");
+        Label condition = new Label();
+        condition.setText("condition name");
+
+        flowsTilePane.getChildren().addAll(button, name, profile, condition);
     }
 
 }
