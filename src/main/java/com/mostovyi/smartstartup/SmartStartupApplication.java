dddc2e4d9e6e6bf3375dd88692b881f1080c057d
Ivan Mostovyi <ivan.mostovyi.work@gmail.com>
2022-08-26 00:41:08 +0300

    Enabled Spring Scheduling

--- src/main/java/com/mostovyi/smartstartup/SmartStartupApplication.java
+++ src/main/java/com/mostovyi/smartstartup/SmartStartupApplication.java
@@ -2,7 +2,9 @@ package com.mostovyi.smartstartup;
 
 import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SmartStartupApplication {
 
